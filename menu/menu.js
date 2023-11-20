4e452118341db35277f2b0860b288820d612d926
ArturMartinsFerreira <artur.ferreira@estudante.ifgoiano.edu.br>
2023-11-20 12:50:45 -0300

    teste 8.6

--- menu/menu.js
+++ menu/menu.js
@@ -1,5 +1,5 @@
 async function addMenu() {
-    const menu = await fetch('/menu/menu.html');
+    const menu = await fetch('/programacao-web-1/menu/menu.html');
     const menuHtml = await menu.text();
     document.body.insertAdjacentHTML("afterbegin", menuHtml);
   }
